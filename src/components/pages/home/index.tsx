084c15f40424769c2f995c30d886e468fcfab020
webyosupernow@gmail.com <webyosupernow@gmail.com>
2024-03-25 13:17:08 +0000

    home fix
    figmaのリンク修正等

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,84 +1,33 @@
 import { formatDate } from "@/utils/common";
-import useSWR from "swr";
-import { request } from "graphql-request";
-
 import { type ReactElement } from "react";
 import { Headline } from "@/components/ui_elements/headline";
 import Styles from "./home.module.css";
-import { ProgressIndicator } from "@/components/ui_parts/progress_indicator";
-import { Divider } from "@/components/ui_elements/divider";
-
-const fetcher = <T,>(query: string): Promise<T> =>
-  request("http://localhost/API/query", query);
-// const fetcher: any = (query: any) => request("https://swapi-graphql.netlify.app/.netlify/functions/index", query);
 
 export const Home = (): ReactElement => {
-  const { data, error } = useSWR(
-    `
-    query {
-        db_ver
-      }
-    `,
-    fetcher,
-  );
-  // const { data, error } = useSWR(
-  //   `
-  //   query Query {
-  //     allFilms {
-  //       films {
-  //         title
-  //         director
-  //         releaseDate
-  //         speciesConnection {
-  //           species {
-  //             name
-  //             classification
-  //             homeworld {
-  //               name
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  //   `,
-  //   fetcher,
-  // );
-  console.log(data);
-  console.log(error);
-
   return (
     <section className={Styles.large_section}>
-      <Headline size="xl">React Vite Project</Headline>
+      <Headline size="xl">React Template</Headline>
       <Headline size="m">&lt; Home &gt;</Headline>
-      <ProgressIndicator />
       <p>{formatDate(new Date(), "yyyy/MM/dd/SSSSS")}</p>
       <p>
-        React Vite Project React Vite Project React Vite Project React Vite
-        Project React Vite Project React Vite Project React Vite Project React
-        Vite Project React Vite Project React Vite Project React Vite Project
-        React Vite Project React Vite Project React Vite Project React Vite
-        Project React Vite Project React Vite Project React Vite Project React
-        Vite Project React Vite Project React Vite Project React Vite Project
-        React Vite Project React Vite Project React Vite Project React Vite
-        Project React Vite Project React Vite Project React Vite Project React
-        Vite Project React Vite Project React Vite Project React Vite Project
-        React Vite Project React Vite Project React Vite Project React Vite
-        Project React Vite Project
+        下記のデザインシステム（Figma）を基にReactに落とし込んだテンプレートです。
+        <br />
+        ソースは
+        <a href="https://github.com/m-eiichi/react_template">
+          https://github.com/m-eiichi/react_template
+        </a>
+        に公開しています。
         <br />
         <br />
       </p>
-      <Divider type="fit" vMarginType="list" />
-      <br />
-      <Divider type="inset" vMarginType="contents" />
       <ul role="list">
         <li>
-          <a href="https://www.figma.com/proto/OILfaxYzcJlpD786Zz0fll/DIC-Design-System?page-id=1%3A5&type=design&node-id=25-499&viewport=515%2C321%2C0.09&t=lE7OQ38rnnEXSAWI-1&scaling=min-zoom&mode=design">
+          <a href="https://www.figma.com/proto/9QaLrBpJaa2oES5qbLLnwH/figma_for_react_template?page-id=58%3A18398&type=design&node-id=2027-8835&viewport=216%2C360%2C0.13&t=RN4tFegZpUA35Gjr-1&scaling=min-zoom&mode=design">
             スタイル-ルール
           </a>
         </li>
         <li>
-          <a href="https://www.figma.com/proto/OILfaxYzcJlpD786Zz0fll/DIC-Design-System?page-id=25%3A2207&type=design&node-id=25-6177&viewport=2830%2C1895%2C0.34&t=6k4VsTTG0Iicnt3K-1&scaling=min-zoom">
+          <a href="https://www.figma.com/proto/9QaLrBpJaa2oES5qbLLnwH/figma_for_react_template?page-id=7%3A11&type=design&node-id=2027-8645&viewport=-7145%2C-5171%2C0.5&t=RMVagOKwXRt8AnZm-1&scaling=min-zoom&mode=design">
             コンポーネント-ルール
           </a>
         </li>
